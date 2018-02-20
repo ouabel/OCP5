a4274a89dd6af954b23535de28b36f75795c861e
dzAhmed <ouabel@live.com>
2018-02-20 19:58:07 +0100

    Installing FOSUserBundle and integrating it with EasyAdminBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new Vich\UploaderBundle\VichUploaderBundle(),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/AppBundle/Controller/Admin/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Controller\Admin;
+
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
+
+class UserController extends BaseAdminController
+{
+    public function createNewUserEntity()
+    {
+        return $this->get('fos_user.user_manager')->createUser();
+    }
+
+    public function persistUserEntity($user)
+    {
+        $this->get('fos_user.user_manager')->updateUser($user, false);
+        parent::persistEntity($user);
+    }
+}
