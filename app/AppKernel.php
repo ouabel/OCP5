63cdeae377559fd5500338695ffc43cf1e782c95
dzAhmed <ouabel@live.com>
2018-02-20 14:07:42 +0100

    Installing StofDoctrineExtensionsBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
 
             new EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
