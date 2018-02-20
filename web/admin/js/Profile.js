a16e2d9b4925d881f6933472874f3b67be38ff1c
dzAhmed <ouabel@live.com>
2018-02-20 17:49:04 +0100

    Adding jquery-locationpicker to locate profiles

--- web/admin/js/Profile.js
+++ web/admin/js/Profile.js
@@ -24,4 +24,19 @@ $(document).ready(function(){
 		loadDistricts(jsDistrict, province);
 	});
 
+	var latitudeInput = $('.js-latitude > div > div > input');
+	var longitudeInput = $('.js-longitude > div > div > input');
+
+	$('.js-pick-location').locationpicker({
+		location: {
+			latitude: latitudeInput.val(),
+			longitude: longitudeInput.val()
+		},
+		radius: 300,
+		inputBinding: {
+			latitudeInput: latitudeInput,
+			longitudeInput: longitudeInput
+		}
+	}).height('400px');;
+
 });
